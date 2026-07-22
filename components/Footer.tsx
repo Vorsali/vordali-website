8dd1fb1e3aae0b56a46eeeb5dd2630842fa655db
Vordali <smothers53@gmail.com>
2026-07-22 05:56:13 -0500

    Add files via upload

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,12 +5,9 @@ export function Footer() {
     <footer className="site-footer platform-footer">
       <Link className="footer-brand" href="/">VORDALI <small>INC.</small></Link>
       <p>Focused software for measurable business problems.</p>
-      <div>
-        <Link href="/products">Products</Link>
-        <Link href="/pricing">Pricing</Link>
-        <Link href="/labs">Labs</Link>
-        <Link href="/why-vordali">Manifesto</Link>
-        <Link href="/trust">Trust</Link>
+      <div className="footer-link-groups">
+        <span><b>Explore</b><Link href="/products">Products</Link><Link href="/pricing">Pricing</Link><Link href="/labs">Labs</Link><Link href="/trust">Trust</Link></span>
+        <span><b>Legal & Compliance</b><Link href="/privacy">Privacy</Link><Link href="/terms">Terms</Link><Link href="/cookies">Cookies</Link><Link href="/sms-consent">SMS Consent</Link></span>
       </div>
       <small>© {new Date().getFullYear()} Vordali Inc. All rights reserved.</small>
     </footer>
